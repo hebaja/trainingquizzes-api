3d4e4f7a7d378719bf7df3bf2d77954c22095a07
hebaja <hebaja@yahoo.com.br>
2022-08-04 01:32:53 -0300

    checking subjects loading tasks

--- src/main/java/com/trainingquizzes/english/model/User.java
+++ src/main/java/com/trainingquizzes/english/model/User.java
@@ -15,6 +15,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
